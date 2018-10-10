09b2bab7093c16f755a4bad012e7721b0b9ffc58
davidPesava <pesava.david@gmail.com>
2018-10-10 17:53:28 +0200

    Add state update

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,40 +1,62 @@
 import React from "react";
 
 import Record from "../components/Record"
-import ButtonAddRecord from "../components/ButtonAddRecord"
 
 import data from "../data.json"
 
 
-const App = () => (
-    <div>
-        <header>
-            <h1>basic page for Wallet app</h1>
-        </header>
+class App extends React.Component {
 
-        <section>
+    constructor(props) {
+        super(props);
+        this.state = {data: data};
+    }
+
+    addRecord = () => {
+
+        let newData = data;
+        newData.push({
+            "name": "Inventi",
+            "type": "income",
+            "value": 643,
+            "created": 1487911188000,
+            "id": 1487911188000
+        });
+
+        this.setState({data: newData})
+    }
+    render() {
+
+        return (
+
+        <div>
+            <button  onClick={this.addRecord}>Add Record</button>
+
+            <header>
+                <h1>basic page for Wallet app</h1>
+            </header>
+
+            <section>
 
 
 
-            <div className="buttonSection">
-                <ButtonAddRecord/>
-            </div>
 
 
+                <div className="records">
 
-            <div className="records">
+                    {data.map(item => (
+                        <Record name={item.name} id={item.id} created={item.created} type={item.type}
+                                value={item.value}/>
+                    ))}
 
-            {data.map(item => (
-               <Record name={item.name} id={item.id} created={item.created} type={item.type} value={item.value}/>
-            ))}
-
-            </div>
-
-
-        </section>
+                </div>
 
 
-    </div>
-);
+            </section>
+
+
+        </div>
+        ) }
+}
 
 export default App;
